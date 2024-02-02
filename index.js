83270f7ba424aaf28cbe7c7c2feac230d6541185
Leonardo Balbastro <leonardonbalbastro@gmail.com>
2024-02-02 13:21:52 -0300

    arreglos esteticos importantes mas agregado de funcionalidad de formulario, se quito la geolocalizacion del mapa de google y se reestructuro el html y los estilos para que sean mas acordes a diferentes dispositivos

--- index.js
+++ index.js
@@ -9,46 +9,57 @@ function irAGitHub(){
 
 }
 
-//funcion para limpiar el formulario luego del post
+if(window.location.href.includes('contact')){
 
-const formulario = document.getElementById('miForm');
+  //funcion para limpiar el formulario luego del post
+  const formulario = document.getElementById('miForm');
+  
+  
+  formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    // Crea una instancia de FormData para obtener los datos del formulario
+    const formData = new FormData(formulario);
+  
+    // Realiza la solicitud POST a Formspree
+    fetch(formulario.getAttribute('action'), {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            // Limpiar los campos del formulario después de enviarlos con éxito
+            clearFormFields();
+            return response.json();
+        }
+        throw new Error('Error en la solicitud.');
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+  });
+  
+  function clearFormFields() {
+    const elements = formulario.elements;
+    for(let i =0 ; i< elements.length;i++){
+      const element = elements[i];
+  
+      if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA' || element.tagName === 'SELECT') {
+        // Limpia el valor del elemento
+        element.value = '';
+    }
+    }
+  }
+  const numberInput = document.getElementById('telefono');
 
-formulario.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  // Crea una instancia de FormData para obtener los datos del formulario
-  const formData = new FormData(formulario);
-
-  // Realiza la solicitud POST a Formspree
-  fetch(formulario.getAttribute('action'), {
-      method: 'POST',
-      body: formData,
-      headers: {
-          'Accept': 'application/json'
-      }
-  })
-  .then(response => {
-      if (response.ok) {
-          // Limpiar los campos del formulario después de enviarlos con éxito
-          clearFormFields();
-          return response.json();
-      }
-      throw new Error('Error en la solicitud.');
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+numberInput.addEventListener('keydown', (event) => {
+    const key = event.key;
+    if (key === 'ArrowUp' || key === 'ArrowDown') {
+        event.preventDefault();
+    }
 });
-
-function clearFormFields() {
-  const elements = formulario.elements;
-  for(let i =0 ; i< elements.length;i++){
-    const element = elements[i];
-
-    if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA' || element.tagName === 'SELECT') {
-      // Limpia el valor del elemento
-      element.value = '';
-  }
-  }
 }
 
 
@@ -76,3 +87,5 @@ function descargarCV(){
   const pdfURL = './docs/Leonardo Balbastro CV.docx';
 
 }
+
+
